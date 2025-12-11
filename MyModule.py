8fb2b131668ea1ab25bbec8de2ad7842a0f37dff
opilane <opilane@HP-CZC2349HV0>
2025-12-11 11:25:18 +0200

    ll,

---
+++ MyModule.py
@@ -0,0 +1,14 @@
+
+
+
+k=['Mari','Juku','Kati']
+s=['1234','5678','abcd']
+
+def registreerimine(k:list,s:list)->any:
+    
+def Autoriseerimine(k:list,s:list)->any:
+    pass
+def paroolivahetus(k:list,s:list)->any:
+    pass
+def paroolitaastamine(k:list,s:list)->any:
+    pass
