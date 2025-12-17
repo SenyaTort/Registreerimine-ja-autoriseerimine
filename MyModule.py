4d1434c1506580d1547c3bee280cd1e27ba6962c
arsen <arsen@ARSENIPC>
2025-12-17 20:28:18 +0200

    Ad

--- MyModule.py
+++ MyModule.py
@@ -1,4 +1,3 @@
-from operator import index
 
 k=['Mari','Juku','Kati']
 s=['1234','5678','abcd']
@@ -6,13 +5,13 @@ s=['1234','5678','abcd']
 def registreerimine(k:list,s:list)->any:
     while True:
         try:
-            kasutaja=input(str("Sisesta kasutaja nimi: "))
+            kasutaja=input("Sisesta kasutaja nimi: ")
             if k.count(kasutaja)==1:
                 print("See login on olemas")
             elif k.count(kasutaja)==0:
-                k.append([kasutaja])
+                k.append(kasutaja)
                 parool=input("Sisesta parool: ")
-                s.append([parool])
+                s.append(parool)
                 print("parool on loodunud")
                 break
         except:
@@ -25,6 +24,8 @@ def Autoriseerimine(k:list,s:list)->any:
                parool2=input("Login on olemas, Sisesta parool: ")
                if s.count(parool2)==1:
                   print("olete sisse logitud")
+               else:
+                   print("Vale parool")
             else:
                 print("See login ei ole")
             break
@@ -33,11 +34,31 @@ def Autoriseerimine(k:list,s:list)->any:
 def paroolivahetus(k:list,s:list)->any:
     while True:
         try:
-            s.remove("Sisesta vana parool: ")
-            parool==
-            input("Sisesta uue parool: ")
-
+            kasutaja=input("Sisesta kasutaja nimi: ")
+            if k.count(kasutaja)==1:
+               vana_parool=input("Sisesta vana parool: ")
+               if s.count(vana_parool)==1:
+                   uus_parool=input("Sisesta uus parool: ")
+                   s.remove(vana_parool)
+                   s.append(uus_parool)
+                   print("Parool on vahetatud")
+               else:
+                   print("Vale parool")
+            else:
+                print("See login ei ole")
+            break
         except:
             ValueError
 def paroolitaastamine(k:list,s:list)->any:
-    pass
+    while True:
+        try:
+            kasutaja=input("Sisesta kasutaja nimi: ")
+            if k.count(kasutaja)==1:
+               uus_parool=input("Sisesta uus parool: ")
+               s.append(uus_parool)
+               print("Parool on taastatud")
+            else:
+                print("See login ei ole")
+            break
+        except:
+            ValueError
