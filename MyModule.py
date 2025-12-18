5c8d2c780ff516a4be72e988c18e029a17ab7eef
opilane <opilane@HP-CZC2349HV0>
2025-12-18 14:03:51 +0200

    fsaf

--- MyModule.py
+++ MyModule.py
@@ -1,21 +1,45 @@
+from random import  *
+import string
+from random import choice
 
-k=['Mari','Juku','Kati']
-s=['1234','5678','abcd']
+k=[]
+s=[]
 
 def registreerimine(k:list,s:list)->any:
-    while True:
-        try:
-            kasutaja=input("Sisesta kasutaja nimi: ")
-            if k.count(kasutaja)==1:
-                print("See login on olemas")
-            elif k.count(kasutaja)==0:
-                k.append(kasutaja)
-                parool=input("Sisesta parool: ")
-                s.append(parool)
-                print("parool on loodunud")
-                break
-        except:
-            ValueError
+    print("1-Jah")
+    print("2-Ei")
+    Gener=input("Kas sa tahad parooli generereida(1/2)? ")
+    if Gener== "1":
+        while True:
+            try:
+                kasutaja=input("Sisesta kasutaja nimi: ")
+                if k.count(kasutaja)==1:
+                    print("See login on olemas")
+                elif k.count(kasutaja)==0:
+                    k.append(kasutaja)
+                    parool=input("Sisesta parool: ")
+                    s.append(parool)
+                    print("parool on loodunud")
+                    break
+            except:
+                ValueError
+    if Gener== "2":
+        while True:
+            try:
+                import random
+                str0 = ".,:;!_*-+()/#¤%&"
+                str1 = '0123456789'
+                str2 = 'qwertyuiopasdfghjklzxcvbnm'
+                str3 = str2.upper()
+                str4 = str0 + str1 + str2 + str3
+                ls = list(str4)
+                random.shuffle(ls)
+                psword = ''.join([random.choice(ls) for x in range(12)])
+                print(psword)
+            except:
+                ValueError
+    else:
+        print("Sisesta numbriga")
 def Autoriseerimine(k:list,s:list)->any:
     while True:
         try:
@@ -27,7 +51,7 @@ def Autoriseerimine(k:list,s:list)->any:
                else:
                    print("Vale parool")
             else:
-                print("See login ei ole")
+                print("See kasutaja ei ole")
             break
         except:
             ValueError
